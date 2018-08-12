b6505d4e960f2252f3c0614ac7bc46d726b7d781
Randall Wood <297232+rhwood@users.noreply.github.com>
2018-08-12 09:22:33 -0500

    Use separate class file for server from launcher.

--- packages/ctc-server/src/index.ts
+++ packages/ctc-server/src/index.ts
@@ -2,29 +2,29 @@ import {Command, flags} from '@oclif/command'
 import {CtcServer} from './CtcServer'
 
 class CtcServerCli extends Command {
-  static description = 'describe the command here'
+  static description = 'Centralized Traffic Control server for model railroads'
+
+  // do not list server in help contents
+  static hidden = true
 
   static flags = {
+    help: flags.help({char: 'h'}),
+    daemon: flags.boolean({char: 'd', allowNo: true, description: 'run as a daemon'}),
+    force: flags.boolean({char: 'f'}),
+    port: flags.string({char: 'p', description: 'use port for remote control'}),
+    socket: flags.string({char: 's', description: 'use socket for local control'}),
     // add --version flag to show CLI version
     version: flags.version({char: 'v'}),
-    help: flags.help({char: 'h'}),
-    // flag with a value (-n, --name=VALUE)
-    name: flags.string({char: 'n', description: 'name to print'}),
-    // flag with no value (-f, --force)
-    force: flags.boolean({char: 'f'}),
   }
 
-  static args = [{name: 'file'}]
+  static args = [{name: 'project'}]
 
   async run() {
     const {args, flags} = this.parse(CtcServerCli)
 
-    const name = flags.name || 'world'
-    this.log(`hello ${name} from ./src/index.ts`)
-    if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`)
-    }
-    const server = new CtcServer('127.0.0.1', 3000, '')
+    const port = (flags.port) ? flags.port : '0'
+    const socket = (flags.socket) ? flags.socket : ''
+    const server = new CtcServer('127.0.0.1', Number(port), socket)
     server.start()
   }
 }
