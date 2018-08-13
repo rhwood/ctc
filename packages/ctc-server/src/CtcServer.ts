28f076674f76787e41f34bed23e3280ddb07c964
Randall Wood <297232+rhwood@users.noreply.github.com>
2018-08-13 02:24:51 -0400

    Initial IPC

--- packages/ctc-server/src/CtcServer.ts
+++ packages/ctc-server/src/CtcServer.ts
@@ -1,5 +1,5 @@
 import http = require('http')
-import {IPC} from 'node-ipc'
+let ipc = new (require('node-ipc')).IPC()
 
 export class CtcServer {
 
@@ -11,18 +11,38 @@ export class CtcServer {
     response.setHeader('Content-Type', 'text/plain');
     response.end('Hello World\n');
   })
-  readonly ipcServer: IPC.Server
+  readonly ipcServer: any
 
   constructor(hostname: string, port: number, socket: string) {
     this.hostname = hostname
     this.port = port
     this.socket = socket
+    if (this.port) {
+      ipc.serveNet(this.hostname, this.port, this.ipcServerCallback)
+    } else {
+      ipc.serve(this.socket, this.ipcServerCallback)
+    }
+    this.ipcServer = ipc.server
   }
 
   start() {
     this.server.listen(this.port, this.hostname, () => {
       console.log(`Server running at http://${this.hostname}:${this.port}/`)
     })
-
+    this.ipcServer.start()
   }
+
+  stop() {
+    this.server.close()
+    this.ipcServer.stop()
+  }
+
+  ipcServerCallback() {
+    ipc.server.on(
+      'stop', function(data: any, socket: any) {
+        stop()
+      }
+    )
+  }
+ 
 }
