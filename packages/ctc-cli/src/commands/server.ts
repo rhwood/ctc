92fc0985495cb504149cfca02688d087f0f6de9d
Randall Wood <297232+rhwood@users.noreply.github.com>
2018-10-15 05:17:57 -0400

    Init project if not daemon

--- packages/ctc-cli/src/commands/server.ts
+++ packages/ctc-cli/src/commands/server.ts
@@ -1,4 +1,8 @@
 import {Command, flags} from '@oclif/command'
+import * as fs from 'fs-extra'
+import * as Path from 'path'
+import cli from 'cli-ux'
+import Init from './init'
 
 export default class Server extends Command {
   static description = 'Control a CTC server'
@@ -11,10 +15,15 @@ export default class Server extends Command {
     daemon: flags.boolean({char: 'd', allowNo: true, description: 'run as a daemon'}),
     force: flags.boolean({char: 'f'}),
     port: flags.string({char: 'p', description: 'use port for remote control'}),
-    socket: flags.string({char: 's', description: 'use socket for local control'}),
+    socket: flags.string({char: 's', description: 'use socket for local control'})
   }
 
   static args = [{name: 'project'}]
+
+  private isProject(project: string): boolean {
+    let properties: string = Path.join(project, 'project.json')
+    return fs.existsSync(properties)
+  }
 
   async run() {
     const {args, flags} = this.parse(Server)
@@ -24,7 +33,20 @@ export default class Server extends Command {
     }
 
     if (this.config.debug) {
-      this.log('Starting server...')
+      this.log('Preparing server...')
+    }
+
+    if (!this.isProject(args.project)) {
+      if (!args.daemon) {
+        this.log(`${args.project} is not a project.`)
+        if (await cli.confirm('Initialize a project?')) {
+          await Init.run([args.project])
+        } else {
+          this.exit()
+        }
+      } else {
+        this.error(`${args.project} is not a project.`)
+      }
     }
   }
 }
