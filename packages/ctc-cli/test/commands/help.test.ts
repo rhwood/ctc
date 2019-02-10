51800cb06c21a559de475422ed91d0f0c2edf344
Randall Wood <297232+rhwood@users.noreply.github.com>
2019-02-10 09:13:46 -0500

    A test that passes.
    The passing test is still just boilerplate, but its enough to begin testing with.

---
+++ packages/ctc-cli/test/commands/help.test.ts
@@ -0,0 +1,11 @@
+import {expect, test} from '@oclif/test'
+
+describe('help', () => {
+  test
+  .stdout()
+  .command(['help'])
+  .it('runs help', ctx => {
+    expect(ctx.stdout).to.contain('Centralized Traffic Control for Model Railroads')
+    expect(ctx.stdout).to.contain('$ ctc [COMMAND]')
+  })
+})
