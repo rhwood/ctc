91536d14fbe10f35ccb35dfbedb0af1cec91913b
Randall Wood <297232+rhwood@users.noreply.github.com>
2019-02-17 10:19:58 -0500

    feat: respect control port and socket settings

--- packages/ctc-cli/src/commands/server.ts
+++ packages/ctc-cli/src/commands/server.ts
@@ -16,8 +16,16 @@ export default class Server extends Command {
   static flags = {
     help: flags.help({char: 'h'}),
     daemon: flags.boolean({char: 'd', allowNo: true, description: 'run as a daemon'}),
-    port: flags.string({char: 'p', description: 'use port for remote control'}),
-    socket: flags.string({char: 's', description: 'use socket for local control'})
+    port: flags.string({
+      char: 'p',
+      description: 'use networkable port for server control',
+      exclusive: ['socket'],
+    }),
+    socket: flags.string({
+      char: 's',
+      description: 'use local socket for server control',
+      exclusive: ['port'],
+    })
   }
 
   static args = [{name: 'project', default: Path.resolve()}]
@@ -27,19 +35,42 @@ export default class Server extends Command {
 
     this.debug('Preparing server...')
 
+    if (flags.daemon) {
+      this.runDaemon(args, flags)
+    } else {
+      this.runNoDaemon(args, flags)
+    }
+  }
+
+  runDaemon(args: any, flags: any) {
     if (!CtcProject.isProject(args.project)) {
-      if (flags.daemon) {
-        this.log(`${args.project} is not a project.`)
-        if (await cli.confirm('Initialize a project?')) {
-          await Init.run([args.project])
-        } else {
-          this.exit()
-        }
+      this.log(`${args.project} is not a project.`)
+      if (cli.confirm('Initialize a project?')) {
+        Init.run([args.project])
       } else {
-        this.error(`${args.project} is not a project.`)
+        this.exit()
       }
     }
+    new CtcServer(this.getProject(args, flags), this.config).start()
+  }
 
-    new CtcServer(new CtcProject(args.project), this.config).start()
+  runNoDaemon(args: any, flags: any) {
+    if (CtcProject.isProject(args.project)) {
+      new CtcServer(this.getProject(args, flags), this.config).start()
+    } else {
+      this.error(`${args.project} is not a project.`)
+    }
+  }
+
+  getProject(args: any, flags: any): CtcProject {
+    let project = new CtcProject(args.project)
+    if (flags.port) {
+      project.config.control.port = flags.port
+    }
+    if (flags.socket) {
+      project.config.control.port = 0
+      project.config.control.socket = flags.socket
+    }
+    return project
   }
 }
